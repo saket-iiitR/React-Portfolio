a2ad9bea0617706694b28cfa20df715dc11b8d0b
Saket Tiwari <code.me.chef@gmail.com>
2021-05-20 14:28:18 +0530

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://saket-iiitr.github.io/My_Portfolio/",
+    "name": "Saket Tiwari",
+    "role": "Devops",
+    "linkedinId":"https://in.linkedin.com/in/saket2307",
+    "skypeid": "Saket23",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://in.linkedin.com/in/saket2307",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/saket-iiitR",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://github.com/saket-iiitR",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a final year student with an excellent academic record and a keen interest and practical exposure in the field of Software Development. I am a super nerd who loves VS Code, Linux, and C++ and enjoys to customize all of the development environment.",
+    "address":"India",
+    "website":"https://saket-iiitr.github.io/My_Portfolio/",
+    "education":[
+      {
+        "UniversityName":"Indian Institute of Information Technology, Ranchi",
+        "specialization":"Electronics and Communication Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2021",
+        "Achievements":"HackWithInfy Top 108"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Rakuten India",
+        "specialization":"Technical Intern",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2050",
+        "Achievements":"Engineering Manager",
+      },
+      {
+        "CompanyName":"Totality Corp",
+        "specialization":"Game Development Intern",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2021",
+        "Achievements":"Launched beta version of Obscurum"
+      }
+    ],
+    "skillsDescription":"I am comfortable with latest technologies",
+    "skills":[
+      {
+        "skillname":"Jenkins"
+      },
+      {
+        "skillname":"Docker"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Mobile App Development",
+        "imgurl":"src/images/app.png"
+      },
+      {
+        "name":"project2",
+        "description":"Web App Development",
+        "imgurl":"src/images/web.png"
+      },
+      {
+        "name":"project3",
+        "description":"Machine Learning",  
+        "imgurl":"src/images/machine.png"
+      },
+      {
+        "name":"project4",
+        "description":"machine learning",
+        "imgurl":"src/images/game.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"He is very dedicated.",
+        "name":"Anshul Rustaggi"
+      },
+      {
+        "description":"Nice Human",
+        "name":"Anshuman Tripathi"
+      }
+    ]
+  }
+  
+  export default resumeData
